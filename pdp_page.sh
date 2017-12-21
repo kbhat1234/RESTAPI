ca83101e19fb5391909c72fda089ed409b6c02a1
kbhat1234 <32067283+kbhat1234@users.noreply.github.com>
2017-12-21 18:09:36 +0530

    Update pdp_page.sh
    update

--- pdp_page.sh
+++ pdp_page.sh
@@ -1,1 +1,1 @@
-curl -i -X GET 'node.licious.in/api/page/productdetails?product_id=pr_57234c427648b&hub_id=1'
+curl -i -X GET 'node.licious.in/api/page/productdetails?product_id=pr_57234c427648b&hub_id=1' -H "token:tk_25dc9c2c9399"
