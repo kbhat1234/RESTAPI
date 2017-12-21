e270884d4c26e9c02929b568fa5e83ab1ebb285c
kbhat1234 <32067283+kbhat1234@users.noreply.github.com>
2017-12-21 15:10:27 +0530

    Create pdp_live.sh
    adding pdp_live.sh

---
+++ pdp_live.sh
@@ -0,0 +1,9 @@
+curl -i -X GET 'node.licious.in/api/mock/page/productdetails?product_id=pr_57234c427648b&hub_id=1'
+
+curl -i -X GET 'node.licious.in/api/mock/catalog/product?product_id=pr_57234a4f6f77b&hub_id=1'
+
+curl -i -X GET 'node.licious.in/api/mock/page/productdetails/offers?product_id=pr_57234a4f6f77b&hub_id=1&source=android'
+
+curl -i -X GET 'node.licious.in/api/mock/page/productdetails/offers?product_id=pr_57234a4f6f77b&hub_id=1&source=ios'
+
+curl -i -X GET 'node.licious.in/api/mock/page/productdetails/prev-next?cat_id=1&hub_id=1'
