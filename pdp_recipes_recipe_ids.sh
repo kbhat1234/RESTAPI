465714809a05bba85e9039071298017fedf37535
kbhat1234 <32067283+kbhat1234@users.noreply.github.com>
2017-12-21 17:59:03 +0530

    Update pdp_recipes_recipe_ids.sh
    update

--- pdp_recipes_recipe_ids.sh
+++ pdp_recipes_recipe_ids.sh
@@ -1,1 +1,1 @@
-curl -i -X GET 'node.licious.in/api/recipes?recipe_ids=re_57112ab52f7ef&recipe_ids=re_5711fef32fb7c&recipe_ids=re_57121c9b3572b'
+curl -i -X GET 'node.licious.in/api/recipes?recipe_ids=re_57112ab52f7ef&recipe_ids=re_5711fef32fb7c&recipe_ids=re_57121c9b3572b' -H "token:tk_25dc9c2c9399"
