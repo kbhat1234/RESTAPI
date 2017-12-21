652d0eff68a7812a990706689dccaf844db78483
kbhat1234 <32067283+kbhat1234@users.noreply.github.com>
2017-12-21 17:30:10 +0530

    Update pdp_product_id.sh
    update

--- pdp_product_id.sh
+++ pdp_product_id.sh
@@ -1,1 +1,1 @@
-curl -i -X GET 'node.licious.in/api/catalog/product/id/chicken-breast-boneless'
+curl -i -X GET 'node.licious.in/api/catalog/product/id/chicken-breast-boneless' -H {'token':'tk_25dc9c2c9399'}
