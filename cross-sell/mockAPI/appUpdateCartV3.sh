d85bdc7dd3d3672b6ddfaa6f4a7c0baa0e075fb1
kbhat1234 <32067283+kbhat1234@users.noreply.github.com>
2018-01-01 12:47:35 +0530

    Update appUpdateCartV3.sh
    v2

--- cross-sell/mockAPI/appUpdateCartV3.sh
+++ cross-sell/mockAPI/appUpdateCartV3.sh
@@ -1,1 +1,1 @@
-curl -i -X POST 'node.licious.in/api/cart/update/v3' -H "token:tk_27d3d4e4ac60" -H "source:mobilesite" -d "customer_key=arshad1234&product_id=pr_57234a4f6f77b&hub_id=1&quantity=1&crosssell_id=someid"
+curl -i -X POST 'node.licious.in/api/cart/update/v2' -H "token:tk_27d3d4e4ac60" -H "source:mobilesite" -d "customer_key=arshad1234&product_id=pr_57234a4f6f77b&hub_id=1&quantity=1&crosssell_id=someid"
