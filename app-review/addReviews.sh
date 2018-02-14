5c7378d091d610068f87aec210c7c8fbc330ccaf
kbhat <32067283+kbhat1234@users.noreply.github.com>
2018-02-14 14:07:07 +0530

    Update addReviews.sh
    modified the script

--- app-review/addReviews.sh
+++ app-review/addReviews.sh
@@ -1,11 +1,1 @@
-curl --request POST \
-  --url https://ms1.licious.in/reviews/add \
-  --header 'Content-Type: application/json' \
-  --data '{
-    "order_id": "jbz441pm",
-    "customer_key":"arjuntest",
-    "review_question":"dsd",
-    "items_count":1,
-     "rating":"2",
-    "review_comments":""
-}'
+curl -i -X POST 'https://ms1.licious.in/reviews/add' -H "Content-Type: application/json" -d "order_id=jbz441pm&customer_key=arjuntest&review_question=dsd&items_count=1&rating=2&review_comments=good"
