35499c41c30b83eb18116d69f7a2d8be7d36381b
kbhat <32067283+kbhat1234@users.noreply.github.com>
2018-02-15 17:14:16 +0530

    Update fetchReviews.sh
    update

--- app-review/fetchReviews.sh
+++ app-review/fetchReviews.sh
@@ -1,1 +1,1 @@
-curl -i -X GET 'https://ms1.licious.in/reviews/fetch/arjuntest' -H "Content-Type:application/json"
+curl -i -X GET 'https://ms1.licious.in/reviews/fetch/c_jdk3q3q0' -H "Content-Type:application/json"
