890360f427f3047fa0770962b148184cf8f72069
kbhat <32067283+kbhat1234@users.noreply.github.com>
2018-02-15 17:32:43 +0530

    Update addReviews.sh
    update

--- app-review/addReviews.sh
+++ app-review/addReviews.sh
@@ -1,1 +1,8 @@
-curl -i -X POST 'https://ms1.licious.in/reviews/add' -H "Content-Type: application/json" -d "order_id=jbz441pm&customer_key=arjuntest&review_question=dsd&items_count=1&rating=2&review_comments=good"
+curl -i -X POST 'https://ms1.licious.in/reviews/add' -H "Content-Type: application/json" -d "
+    "order_id": "jdog3eag",
+    "customer_key":"c_jdk3q3q0",
+    "review_question":"how was the product delivered",
+    "items_count":4,
+     "rating":"4",
+    "review_comments":"Very Bad product"
+"
