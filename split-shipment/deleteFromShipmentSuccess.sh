aa240cfe1086a0a8e9e13b86a15bf46f0705a6c9
kbhat <32067283+kbhat1234@users.noreply.github.com>
2018-02-15 12:14:33 +0530

    Update deleteFromShipmentSuccess.sh
    update

--- split-shipment/deleteFromShipmentSuccess.sh
+++ split-shipment/deleteFromShipmentSuccess.sh
@@ -1,1 +1,1 @@
-curl -i -X 'node.licious.in/api/order/shipments/delete/5a817264bbe715bd0a29df44/1/pr_574f256be6c0a?source=mobilesite'
+curl -i -X GET 'node.licious.in/api/order/shipments/delete/5a817264bbe715bd0a29df44/1/pr_574f256be6c0a?source=mobilesite'
