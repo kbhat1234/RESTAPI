a0810196b87fa5c3580fe428c33d902a5e1a1546
kbhat <32067283+kbhat1234@users.noreply.github.com>
2018-02-16 14:24:49 +0530

    Update ignoreReviews.sh
    updated

--- app-review/ignore-reviews/ignoreReviews.sh
+++ app-review/ignore-reviews/ignoreReviews.sh
@@ -1,7 +1,4 @@
-curl --request POST \
-  --url https://ms1.licious.in/reviews/ignore \
-  --header 'Content-Type: application/json' \
-  --data '{
-    "order_id": "jbz441pm",
-    "customer_key":"arjuntest"
+curl -i -X POST 'https://ms1.licious.in/reviews/ignore' -H "Content-Type:application/json" -d '{
+    "order_id": "jdog3eaf",
+    "customer_key":"c_jdk3q3q0"
 }'
