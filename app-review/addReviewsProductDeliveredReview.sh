619e8b725ae4262b46203e5c499af4124d437f6f
kbhat <32067283+kbhat1234@users.noreply.github.com>
2018-02-16 13:17:16 +0530

    Update addReviewsProductDeliveredReview.sh
    updated

--- app-review/addReviewsProductDeliveredReview.sh
+++ app-review/addReviewsProductDeliveredReview.sh
@@ -3,8 +3,8 @@
 #provide valid order_id, customer_key, items_count, rating
 
 curl -i -X POST 'https://ms1.licious.in/reviews/add' -H "Content-Type:application/json" -d '{
-    "order_id": "",
-    "customer_key":"jdog3eaf",
+    "order_id": "jdog3eaf",
+    "customer_key":"c_jdk3q3q0",
     "review_question":"how was the product delivered",
     "items_count":4,
      "rating":"4",
