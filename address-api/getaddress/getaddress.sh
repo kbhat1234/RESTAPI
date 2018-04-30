36ae4f1d7c3326f87894ca64c27d267a9c86a6f4
kbhat1234 <karthikbhat@live.com>
2018-04-30 10:50:41 +0530

    New File

---
+++ address-api/getaddress/getaddress.sh
@@ -0,0 +1,9 @@
+curl -i -X GET "http://52.77.22.148:3001/users/v1/getaddress?customer_key=c_jgdkduzz&hub_id=4" -H 'access-token: tk_jghephjr'
+curl -i -X GET "http://52.77.22.148:3001/users/v1/getaddresss?customer_key=c_jgdkduzz&hub_id=4" -H 'access-token: tk_jghephjr'
+curl -i -X GET "http://52.77.22.148:3001/users/v1/getaddress?customer_key=c_jgdkduzz&hub_id=4" -H 'access-token: 1tk_jghephjr'
+curl -i -X GET "http://52.77.22.148:3001/users/v1/getaddress?customer_key=1c_jgdkduzz&hub_id=4" -H 'access-token: tk_jghephjr'
+curl -i -X GET "http://52.77.22.148:3001/users/v1/getaddress?customer_key=c_jgdkduzz&hub_id=201" -H 'access-token: tk_jghephjr'
+curl -i -X GET "http://52.77.22.148:3001/users/v1/getaddress?customer_key=&hub_id=" -H 'access-token: tk_jghephjr'
+curl -i -X GET "http://52.77.22.148:3001/users/v1/getaddress?" -H 'access-token: tk_jghephjr'
+curl -i -X GET "http://52.77.22.148:3001/users/v1/getaddress?customer_key=c_jgdkduzz&hub_id=4" -H 'access-token: '
+curl -i -X GET "http://52.77.22.148:3001/users/v1/getaddress?customer_key=c_jgdkduzz&hub_id=4"
