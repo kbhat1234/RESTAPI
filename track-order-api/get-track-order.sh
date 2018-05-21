8c0c6f13257aaeccb9d58d5a9d75495f70c61bfb
kbhat1234 <karthikbhat@live.com>
2018-05-21 15:46:50 +0530

    add

---
+++ track-order-api/get-track-order.sh
@@ -0,0 +1,3 @@
+curl -i -X GET 'http://52.77.22.148:3001/users/v1/trackorder?order_id=jhg0iz3r&customer_key=c_jgq53x5k' -H "access-token:tk_jghephjr" -H "source:website"
+curl -i -X GET 'http://52.77.22.148:3001/users/v1/trackorder1?order_id=jhg0iz3r&customer_key=c_jgq53x5k' -H "access-token:tk_jghephjr" -H "source:website"
+
