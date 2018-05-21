9ea186e07fac930f4a94b63903017e51351b405d
kbhat1234 <karthikbhat@live.com>
2018-05-21 16:23:28 +0530

    add

---
+++ track-order-api/get-order-shipments-user.sh
@@ -0,0 +1,23 @@
+curl -i -X GET 'http://52.77.22.148:3001/users/v1/order_shipments?order_id=jhg0iz3r&customer_key=c_jgq53x5k' -H "source:msite" -H "access-token:tk_jghephjr"
+
+curl -i -X GET 'http://52.77.22.148:3001/users/v1/order_shipments1?order_id=jhg0iz3r&customer_key=c_jgq53x5k' -H "source:msite" -H "access-token:tk_jghephjr"
+
+curl -i -X GET 'http://52.77.22.148:3001/users/v1/order_shipments?order_id=jhg0iz31&customer_key=c_jgq53x5k' -H "source:msite" -H "access-token:tk_jghephjr"
+
+curl -i -X GET 'http://52.77.22.148:3001/users/v1/order_shipments?order_id=jhg0iz3r&customer_key=c_jgq53x5k1' -H "source:msite" -H "access-token:tk_jghephjr"
+
+curl -i -X GET 'http://52.77.22.148:3001/users/v1/order_shipments?order_id=jhg0iz3r&customer_key=c_jgq53x5k' -H "source:msites" -H "access-token:tk_jghephjr"
+
+curl -i -X GET 'http://52.77.22.148:3001/users/v1/order_shipments?order_id=jhg0iz3r&customer_key=c_jgq53x5k' -H "source:msite" -H "access-token:tk_jghephjs"
+
+curl -i -X GET 'http://52.77.22.148:3001/users/v1/order_shipments?order_id=&customer_key=' -H "source:msite" -H "access-token:tk_jghephjr"
+
+curl -i -X GET 'http://52.77.22.148:3001/users/v1/order_shipments?order_id=jhg0iz3r&customer_key=c_jgq53x5k'
+
+curl -i -X GET 'http://52.77.22.148:3001/users/v1/order_shipments?' -H "source:msite" -H "access-token:tk_jghephjr"
+
+curl -i -X GET 'http://52.77.22.148:3001/users/v1/order_shipments?'
+
+
+
+
