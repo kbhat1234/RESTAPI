99ef4faa36ab21df5afb15956412fa35624274c7
kbhat1234 <karthikbhat@live.com>
2018-05-28 17:54:32 +0530

    add

--- paylater-api/paylater-validation.sh
+++ paylater-api/paylater-validation.sh
@@ -17,3 +17,5 @@ curl -i -X POST 'http://52.77.22.148:3001/users/v1/paynow' -H "access-token:tk_j
 curl -i -X POST 'http://52.77.22.148:3001/users/v1/paynow1' -H "access-token:tk_jghephjr" -H "source:android" -d "customer_key=c_jgq53x5k&order_id=jhitjaoh&shipment_id=jhitjaoh&wallet_amount=0&payble_amount=234.5&source=android&txt_id=txt-id-001" || no order_type in body
 
 curl -i -X POST 'http://52.77.22.148:3001/users/v1/paynow1' -H "access-token:tk_jghephjr" -H "source:android" -d "customer_key=c_jgq53x5k&order_id=jhitjaoh&shipment_id=jhitjaoh&wallet_amount=0&payble_amount=234.5&source=android&order_type=wallet" || no txt_id in body
+
+curl -i -X POST 'http://52.77.22.148:3001/users/v1/paynow' -H "access-token:tk_jghephjr" -H "source:ANDROID" -d "customer_key=c_jgq53x5k&order_id=jhitjaoh&shipment_id=jhitjaoh&wallet_amount=0&payble_amount=234.5&source=ANDROID&order_type=wallet&txt_id=txt-id-001" || no source in header
