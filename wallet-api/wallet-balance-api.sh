0c87e1652fb6f7d14fc751067a9456ef12d43d17
kbhat1234 <karthikbhat@live.com>
2018-06-01 18:33:44 +0530

    add

---
+++ wallet-api/wallet-balance-api.sh
@@ -0,0 +1,1 @@
+curl -i -X GET 'node.licious.in/api/wallet/balance?phone=9886867677' -H "token:tk_jghephjr"
---
+++ wallet-api/wallet-transaction-history-api.sh
@@ -0,0 +1,1 @@
+curl -i -X GET 'node.licious.in/api/user/wallet/transaction/get?customer_key=c_jgq53x5k' -H "token:tk_jghephjr"
