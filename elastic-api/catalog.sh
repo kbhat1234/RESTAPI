f9f8c2557c1d27ad572641fe6ed48e9a4ddc25cf
kbhat1234 <karthikbhat@live.com>
2018-06-26 19:53:38 +0530

    add

---
+++ elastic-api/catalog.sh
@@ -0,0 +1,1 @@
+curl -i -X GET 'http://node.licious.info/api/catalog/products/all?cat_id=20&hub_id=15&source=mobilesite'
---
+++ elastic-api/elastic_search_query.sh
@@ -0,0 +1,1 @@
+curl -i -X GET 'http://35.229.237.244:9200/_sql?sql=select%20*%20from%20orderdataplan%20limit%202'
